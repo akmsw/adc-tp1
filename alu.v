a25a473d0dea521938b5ce6d55c963fec8367ff2
akmsw <franbonino82@gmail.com>
2022-09-15 12:46:45 -0300

    general maintenance

--- alu.v
+++ alu.v
@@ -1,66 +1,69 @@
-module my_alu#(
-        parameter BUS_SIZE = 8,                 // 8-bits input/output bus
-        parameter OPCODE_SIZE = 6,              // 6-bits operation codes
+module alu
+  #(
+    parameter OPCODE_SIZE = 6,              // 6-bits operation codes
+    parameter BUS_SIZE = 8                 // 8-bits input/output bus
+  )
+  (
+    input wire i_clock,
+    input wire i_boton1,
+    input wire i_boton2,
+    input wire i_boton3,
+    input wire [BUS_SIZE - 1 : 0] i_swiches,
 
-        // ALU opcodes
-        parameter ADD = 6'b100000,
-        parameter SUB = 6'b100010,
-        parameter AND = 6'b100100,
-        parameter OR  = 6'b100101,
-        parameter XOR = 6'b100110,
-        parameter NOR = 6'b100111,
-        parameter SRL = 6'b000010,
-        parameter SRA = 6'b000011
-    )
-     (
-       	input wire i_clock,
-        input [BUS_SIZE - 1 : 0] i_swiches,
-    	input wire i_boton1,
-       	input wire i_boton2,
-       	input wire i_boton3,
-        output [BUS_SIZE - 1 : 0] o_ALUout,
-       	output wire o_carry
-    );
+    output wire o_carry,
+    output wire [BUS_SIZE - 1 : 0] o_ALUout
+  );
+
+  // ALU opcodes
+  localparam ADD = 6'b100000;
+  localparam SUB = 6'b100010;
+  localparam AND = 6'b100100;
+  localparam OR  = 6'b100101;
+  localparam XOR = 6'b100110;
+  localparam NOR = 6'b100111;
+  localparam SRL = 6'b000010;
+  localparam SRA = 6'b000011;
+
+  reg [OPCODE_SIZE - 1 : 0] opcode;
   reg [BUS_SIZE - 1 : 0] datoA;
   reg [BUS_SIZE - 1 : 0] datoB;
-  reg [OPCODE_SIZE - 1 : 0] opcode;
   reg [BUS_SIZE : 0] result;
-  
+
   assign o_carry = result[BUS_SIZE];
   assign o_ALUout = result;
+
   always @(posedge i_clock)
-  
-    begin
-    if(i_boton1)
+  begin
+    if (i_boton1)
       datoA <= i_swiches;
-      
-  	if(i_boton2)
+    else if (i_boton2)
       datoB <= i_swiches;
-      
-  	if(i_boton3)
+    else if (i_boton3)
       opcode <= i_swiches;
-      
-        case (opcode)
-            ADD:
-                result = datoA + datoB;
-            SUB:
-                result = datoA - datoB;
-            AND:
-                result = datoA & datoB;
-            OR:
-                result = datoA | datoB;
-            XOR:
-                result = datoA ^ datoB;
-            NOR:
-                result = ~(datoA | datoB);
-            SRL:
-                result = datoA >> 1;
-            SRA:
-                result = datoA >>> 1;
-            default:
-                result = datoA + datoB;           // Sum by default
-        endcase
-    end
+  end
+
+  always @(*)
+  begin
+    case (opcode)
+        ADD:
+            result = datoA + datoB;
+        SUB:
+            result = datoA - datoB;
+        AND:
+            result = datoA & datoB;
+        OR:
+            result = datoA | datoB;
+        XOR:
+            result = datoA ^ datoB;
+        NOR:
+            result = ~(datoA | datoB);
+        SRL:
+            result = datoA >> 1;
+        SRA:
+            result = datoA >>> 1;
+        default:
+            result = datoA + datoB;           // Sum by default
+    endcase
+  end
 
 endmodule
-
--- testbench.v
+++ testbench.v
@@ -1,89 +1,111 @@
 module alutest();
- // Local parameters
-  localparam BUS_SIZE = 8;
- // Outputs
- wire [BUS_SIZE - 1 : 0] o_ALUout ;
- wire carry;
- // Inputs
- reg [BUS_SIZE - 1 : 0] i_swiches ;
- reg i_boton1 ;
- reg i_boton2 ;
- reg i_boton3 ;
- reg i_clock ;
 
- initial begin $dumpfile("dump.vcd"); $dumpvars;
- #0
-i_swiches = 9'd0;
-i_boton1 = 1'b0;
-i_boton2 = 1'b0;
-i_boton3 = 1'b0;
-i_clock = 1'b0;
-//add
- #1
-i_swiches = 9'd255;
-i_boton1 = 1'b1;
- #1
-i_boton1 = 1'b0;
- #1
-i_swiches = 9'd2;
-i_boton2 = 1'b1;
- #1
-i_boton2 = 1'b0;
- #1
-i_swiches = 6'b100000;
-i_boton3 = 1'b1;
- #1
-i_boton3 = 1'b0;
-//sub 
- #1
-i_swiches = 9'd10;
-i_boton1 = 1'b1;
- #1
-i_boton1 = 1'b0;
- #1
-i_swiches = 9'd4;
-i_boton2 = 1'b1;
- #1
-i_boton2 = 1'b0;
- #1
-i_swiches = 6'b100010;
-i_boton3 = 1'b1;
- #1
-i_boton3 = 1'b0;
-// and    
- #1
-i_swiches = 9'b00110111;
-i_boton1 = 1'b1;
- #1
-i_boton1 = 1'b0;
- #1
-i_swiches = 9'b01100101;
-i_boton2 = 1'b1;
- #1
-i_boton2 = 1'b0;
- #1
-i_swiches = 6'b100100;
-i_boton3 = 1'b1;
- #1
-i_boton3 = 1'b0;   
- #100
- $finish ;
- end
- always begin
- #1
- i_clock = ~i_clock ;
- end
- my_alu
- #(
- )
- my_alu_instance
- (
- .o_ALUout ( o_ALUout ),
- .i_clock ( i_clock ),
- .i_boton1 ( i_boton1 ),
- .i_boton2 ( i_boton2 ),
- .i_boton3 ( i_boton3 ),
- .i_swiches ( i_swiches ),
- .o_carry(carry)
- );
-endmodule // tstb_ejemplo
+// Local parameters
+localparam BUS_SIZE = 8;
+
+// Outputs
+wire [BUS_SIZE - 1 : 0] o_ALUout ;
+wire carry;
+
+// Inputs
+reg [BUS_SIZE - 1 : 0] i_swiches ;
+reg i_boton1 ;
+reg i_boton2 ;
+reg i_boton3 ;
+reg i_clock ;
+
+// ALU opcodes
+localparam ADD = 6'b100000;
+localparam SUB = 6'b100010;
+localparam AND = 6'b100100;
+localparam OR  = 6'b100101;
+localparam XOR = 6'b100110;
+localparam NOR = 6'b100111;
+localparam SRL = 6'b000010;
+localparam SRA = 6'b000011;
+
+initial
+begin
+  $dumpfile("dump.vcd");
+  $dumpvars;
+
+  #0
+  i_swiches = 9'd0;
+  i_boton1 = 1'b0;
+  i_boton2 = 1'b0;
+  i_boton3 = 1'b0;
+  i_clock = 1'b0;
+
+  // ADD
+  #1
+  i_swiches = 9'd255;
+  i_boton1 = 1'b1;
+  #1
+  i_boton1 = 1'b0;
+  #1
+  i_swiches = 9'd2;
+  i_boton2 = 1'b1;
+  #1
+  i_boton2 = 1'b0;
+  #1
+  i_swiches = ADD;
+  i_boton3 = 1'b1;
+  #1
+  i_boton3 = 1'b0;
+
+  // SUB
+  #1
+  i_swiches = 9'd10;
+  i_boton1 = 1'b1;
+  #1
+  i_boton1 = 1'b0;
+  #1
+  i_swiches = 9'd4;
+  i_boton2 = 1'b1;
+  #1
+  i_boton2 = 1'b0;
+  #1
+  i_swiches = SUB;
+  i_boton3 = 1'b1;
+  #1
+  i_boton3 = 1'b0;
+
+  // AND
+  #1
+  i_swiches = 9'b00110111;
+  i_boton1 = 1'b1;
+  #1
+  i_boton1 = 1'b0;
+  #1
+  i_swiches = 9'b01100101;
+  i_boton2 = 1'b1;
+  #1
+  i_boton2 = 1'b0;
+  #1
+  i_swiches = AND;
+  i_boton3 = 1'b1;
+  #1
+  i_boton3 = 1'b0;
+  #100
+  $finish;
+end
+
+always
+begin
+  #1
+  i_clock = ~i_clock ;
+end
+
+alu #()
+alu_instance
+(
+  .o_ALUout ( o_ALUout ),
+  .i_clock ( i_clock ),
+  .i_boton1 ( i_boton1 ),
+  .i_boton2 ( i_boton2 ),
+  .i_boton3 ( i_boton3 ),
+  .i_swiches ( i_swiches ),
+  .o_carry(carry)
+);
+
+endmodule
